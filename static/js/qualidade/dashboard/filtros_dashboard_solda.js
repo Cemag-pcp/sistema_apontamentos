57f6d4a38c81c0e8932b359cc3ae506a1e74955c
Cemag-pcp <pcp@cemag.com.br>
2024-06-14 13:02:56 -0300

    Dash de Estamparia, Pintura e Solda finalizados

--- static/js/qualidade/dashboard/filtros_dashboard_solda.js
+++ static/js/qualidade/dashboard/filtros_dashboard_solda.js
@@ -1,15 +1,19 @@
 function filtrarTabela(filtroId, tabelaId, somaId) {
-    var input = document.getElementById(filtroId).value.toLowerCase();
+    var select = document.getElementById(filtroId);
+    var selectedValues = Array.from(select.selectedOptions).map(option => option.value.toLowerCase());
     var table = document.getElementById(tabelaId);
     var totalElement = document.getElementById(somaId);
     var rows = table.getElementsByTagName('tr');
     var totalSum = 0;
 
+    // Se nenhum valor estiver selecionado, exibir todas as linhas
+    var mostrarTodas = selectedValues.length === 0;
+
     for (var i = 1; i < rows.length; i++) { // start at 1 to skip the header row
         var cells = rows[i].getElementsByTagName('td');
         var causa = cells[1].textContent || cells[1].innerText;
         var quantidade = parseInt(cells[2].textContent || cells[2].innerText, 10);
-        if (causa.toLowerCase().startsWith(input)) {
+        if (mostrarTodas || selectedValues.includes(causa.toLowerCase())) {
             rows[i].style.display = '';
             totalSum += quantidade;
         } else {
@@ -20,14 +24,33 @@ function filtrarTabela(filtroId, tabelaId, somaId) {
     totalElement.textContent = totalSum;
 }
 
-document.getElementById('filtro_total_causas').addEventListener('input', function() {
+$('#filtro_total_causas').on('change', function() {
     filtrarTabela('filtro_total_causas', 'dataTableCausas', 'total_causas');
 });
 
-document.getElementById('filtro_liquida').addEventListener('input', function() {
+$('#filtro_liquida').on('change', function() {
     filtrarTabela('filtro_liquida', 'dataTableCausasLiquida', 'soma_total_liquida');
 });
 
-document.getElementById('filtro_po').addEventListener('input', function() {
+$('#filtro_po').on('change', function() {
     filtrarTabela('filtro_po', 'dataTableCausasPo', 'soma_total_po');
 });
+
+$('#filtro_total_causas').select2({
+    placeholder: "Selecione as causas",
+    allowClear: true,
+    width: 'resolve'
+});
+
+$('#filtro_liquida').select2({
+    placeholder: "Selecione as causas do tipo PU",
+    allowClear: true,
+    width: 'resolve'
+});
+
+$('#filtro_po').select2({
+    placeholder: "Selecione as causas do tipo PÓ",
+    allowClear: true,
+    width: 'resolve'
+});
+
