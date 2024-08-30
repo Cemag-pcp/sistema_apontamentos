e2c884ecaa37a3806554bba7775dca2f08b98f98
Cemag-pcp <pcp@cemag.com.br>
2024-08-30 08:30:30 -0300

    Colocando arquivo selecionando dentro do input

--- static/js/estamparia/input-file.js
+++ static/js/estamparia/input-file.js
@@ -12,3 +12,10 @@ document.querySelector('input[type="file"]').addEventListener('change',function 
     }
 
 })
+
+document.getElementById('inserir-arquivos').addEventListener('change', function(event) {
+    const input = event.target;
+    const label = input.nextElementSibling;
+    const fileName = input.files.length > 0 ? input.files[0].name : 'Inserir arquivo';
+    label.textContent = fileName;
+});
