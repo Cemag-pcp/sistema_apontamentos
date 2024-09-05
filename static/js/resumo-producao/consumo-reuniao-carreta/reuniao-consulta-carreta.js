06c4514e25859991ee7c6278dbf9bd960e381d17
Cemag-pcp <pcp@cemag.com.br>
2024-09-05 17:16:29 -0300

    Criando rota de consumir itens

---
+++ static/js/resumo-producao/consumo-reuniao-carreta/reuniao-consulta-carreta.js
@@ -0,0 +1,42 @@
+document.getElementById('filtrar_datas_por_carretas').addEventListener('click', function() {
+    showLoading();
+    const dataInicial = document.getElementById('dataReuniaoInicial').value;
+    const dataFinal = document.getElementById('dataReuniaoFinal').value;
+
+    // Crie o objeto com os dados a serem enviados
+    const data = {
+        data_inicial: dataInicial,
+        data_final: dataFinal
+    };
+
+    // Envie os dados para o servidor usando fetch
+    fetch('/consultar-carreta-reuniao', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(result => {
+        console.log('Sucesso:', result);
+
+        window.receivedData = result.ImportarDados;
+
+        // Atualize a tabela com os dados recebidos e o filtro atual
+        updateTable();
+        
+        hideLoading();
+        document.getElementById('campoTableConsulta').style.display = 'block';
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        hideLoading();
+    });
+});
+
+document.getElementById('recursoFiltro').addEventListener('input', function() {
+    if (window.receivedData) {
+        updateTable();
+    }
+});
